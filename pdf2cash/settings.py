7378b53f7278f676c80ca83eeea3059c444f961b
jclitwin <j.litwin@live.com>
2018-10-06 19:08:09 -0300

    add django-cors to settings.
    Co-authored-by: Brian2397 <brianlui2387@gmail.com>

--- pdf2cash/settings.py
+++ pdf2cash/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+	'corsheaders',
     'worker'
 ]
 
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'pdf2cash.urls'
@@ -123,3 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = (
+	'0.0.0.0:3000',
+    'localhost:3000',
+    '127.0.0.1:3000'
+)
