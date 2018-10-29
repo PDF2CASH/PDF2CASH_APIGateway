54d1cb60a51f45d07118cf3b6933bea78bdc7e6a
Bruno Matias Casas <leguenta@hotmail.com>
2018-10-29 17:41:49 -0200

    Add authentication system; refs: fga-eps-mds/2018.2-PDF2CASH#128

--- pdf2cash/settings.py
+++ pdf2cash/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders',
-    'worker'
+	'corsheaders',
+    'worker',
+    'apigateway',
 ]
 
 MIDDLEWARE = [
@@ -140,3 +141,14 @@ CORS_ORIGIN_WHITELIST = (
     'localhost:3000',
     '127.0.0.1:3000'
 )
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
