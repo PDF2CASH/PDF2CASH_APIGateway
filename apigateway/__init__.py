c1a03f9ce8a9b56e4d4d2080085dd2e7cfefaf73
Bruno Matias Casas <leguenta@hotmail.com>
2018-10-29 17:41:04 -0200

    Create api gateway app; refs: fga-eps-mds/2018.2-PDF2CASH#128

---
+++ apigateway/__init__.py
@@ -0,0 +1,11 @@
+__title__ = 'Django API Gateway'
+__version__ = '0.1'
+__author__ = 'ECIDI'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2011-2017 ECIDI'
+
+# Version synonym
+VERSION = __version__
+
+def get_version():
+    return __version__
---
+++ apigateway/views.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import requests
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from .models import Api
+from rest_framework.permissions import IsAuthenticated
+
+class gateway(APIView):
+
+    def operation(self, request):
+        path = request.path_info.split('/')
+        if len(path) < 2:
+            return Response('bad request', status=status.HTTP_400_BAD_REQUEST)
+        
+        apimodel = Api.objects.filter(name=path[2])
+        if apimodel.count() != 1:
+            return Response('bad request', status=status.HTTP_400_BAD_REQUEST)
+
+        valid, msg = apimodel[0].check_plugin(request)
+        if not valid:
+            return Response(msg, status=status.HTTP_400_BAD_REQUEST)
+
+        res = apimodel[0].send_request(request)
+        if res.headers.get('Content-Type', '').lower() == 'application/json':
+            data = res.json()
+        else:
+            data = res.content
+        return Response(data=data, status=res.status_code)
+    
+    def get(self, request):
+        return self.operation(request)
+
+    def post(self, request):
+        return self.operation(request)
+
+    def put(self, request):
+        return self.operation(request)
+    
+    def patch(self, request):
+        return self.operation(request)
+    
+    def delete(self, request):
+        return self.operation(request)
