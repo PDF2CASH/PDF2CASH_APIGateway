43a59dd9ab683c7e54301c48874cead82ac21948
Wallacy Braz <wallacy.braz@live.com>
2018-11-28 01:25:28 -0200

    Fix sendZip.sh script

--- scripts/sendZip.sh
+++ scripts/sendZip.sh
@@ -2,6 +2,5 @@
 
 apt get install zip
 apt get install curl
-cd ..
 zip -r  ./apigateway.zip ./build
 curl -F 'file=@./apigateway.zip' https://pdf2cash-cloudupdater.herokuapp.com/api/system/apigateway/
